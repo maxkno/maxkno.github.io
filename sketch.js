81989a5200424066da72cd37fea5940b34e2d87c
Max Knobbe <max.knobbe@stud.uni-goettingen.de>
2019-05-15 16:16:20 +0200

    adding text

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var c_size = 910;
 var size = 10;
 var w;
+var count = 0;
 
 var field;
 
@@ -26,21 +27,31 @@ function draw() {
     if(field.won){
 	field.reset();
     }
+    if(count == 0 && field.finished){
+	textSize(50);
+	fill(255,255,255);
+	text('Reach the upper left corner!', 100, 400);
+    }
 
+    
 }
 
 function keyPressed() {
     if(keyCode == UP_ARROW){
 	field.walk(-1,0);
+	count++;
     }
     if(keyCode == DOWN_ARROW){
 	field.walk(1,0);
+	count++;
     }
     if(keyCode == RIGHT_ARROW){
 	field.walk(0,1);
+	count++;
     }
     if(keyCode == LEFT_ARROW){
 	field.walk(0,-1);
+	count++;
     }
 }
 
